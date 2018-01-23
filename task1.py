0bac37248987fb640375d54ac415bf84cd2c5326
gskirankumar6 <kiran@dal.ca>
2018-01-23 01:35:17 +0000

    just checking my code

---
+++ task1.py
@@ -0,0 +1,13 @@
+import string
+dict1 = dict()
+def count_not_a_article():
+	count  = 0
+	file1 = open("Book-1.txt")
+
+	for line in file1:	
+		a = line.split()
+		for word in a:
+			if word not in ["a", "the", "at", "run", "to","and","are","or","for","an","this"]:
+				count = count + 1
+	print ("total num of words not including articles",count)
+count_not_a_article()
